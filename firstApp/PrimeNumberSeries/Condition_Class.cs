487d6202066fa9e4dd59682d88657bdd85eb8b1f
Zain <zainofficial03@gmail.com>
2021-05-01 00:14:01 +0500

    Prime Number

---
+++ firstApp/PrimeNumberSeries/Condition_Class.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace firstApp.PrimeNumberSeries
+{
+    class Condition_Class
+    {
+        public void Prime(int x)
+        {
+            for(int i = 2; i<=x; i++)
+            {
+                int Divisor = 0;
+                for(int j=1; j<=i; j++)
+                {
+                    if(i%j == 0)
+                    {
+                        Divisor++;
+                    }
+                }
+                if(Divisor == 2)
+                {
+                    Console.WriteLine("{0} is Prime ",i);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is Not Prime ", i);
+                }
+
+            }
+        }
+    }
+}
---
+++ firstApp/PrimeNumberSeries/Main_Class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace firstApp.PrimeNumberSeries
+{
+    class Main_Class
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Any Number");
+            int g = Convert.ToInt32(Console.ReadLine());
+            Condition_Class c = new Condition_Class();
+            c.Prime(g);
+        }
+    }
+}
